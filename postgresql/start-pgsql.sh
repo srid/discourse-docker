432efe45da60815d060f818a36db8bea3bf4ff17
Sridhar Ratnakumar <sridharr@activestate.com>
2013-08-31 01:06:36 +0000

    keep postgres conf local

--- postgresql/start-pgsql.sh
+++ postgresql/start-pgsql.sh
@@ -5,11 +5,22 @@
 DATADIR=/var/lib/postgresql/9.1/main
 BINDIR=/usr/lib/postgresql/9.1/bin
 
-mkdir -p $DATADIR
-chown -R postgres /var/lib/postgresql
+# test if DATADIR is existent
+if [ ! -d $DATADIR ]; then
+  echo "Creating Postgres data at $DATADIR"
+  mkdir -p $DATADIR
+fi
 
 echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.1/main/pg_hba.conf
 
-su postgres sh -c "$BINDIR/initdb $DATADIR"
-su postgres sh -c "$BINDIR/postgres --single  -D $DATADIR  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
+# test if DATADIR has content
+if [ ! "$(ls -A $DATADIR)" ]; then
+  echo "Initializing Postgres Database at $DATADIR"
+  chown -R postgres $DATADIR
+  su postgres sh -c "$BINDIR/initdb $DATADIR"
+  su postgres sh -c "$BINDIR/postgres --single  -D $DATADIR  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$PASS';"
+fi
+
+cat /etc/postgresql/9.1/main/postgresql.conf
+
 su postgres sh -c "$BINDIR/postgres           -D $DATADIR  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*" 
